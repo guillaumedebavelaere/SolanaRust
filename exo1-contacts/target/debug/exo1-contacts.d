ae26f5789f0359019e3e6c5c1aacb63bb12772c9
Guillaume Debavelaere <guillaume.debavelaere@gmail.com>
2025-02-27 21:40:55 +0000

    remove: target

--- exo1-contacts/target/debug/exo1-contacts.d
+++
@@ -1,1 +0,0 @@
-/Users/odomart/Desktop/ALYRA/SolanaRust/exo1-contacts/target/debug/exo1-contacts: /Users/odomart/Desktop/ALYRA/SolanaRust/exo1-contacts/src/main.rs
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/odomart/Desktop/ALYRA/SolanaRust/hello_world/target/debug/hello_world: /Users/odomart/Desktop/ALYRA/SolanaRust/hello_world/src/main.rs
